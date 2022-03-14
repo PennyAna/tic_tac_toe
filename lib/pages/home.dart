501925c0794bd5b5f4eb8de1517ff5904cd08313
Monty Rasmussen <montyr75@gmail.com>
2022-03-14 17:57:35 -0600

    fixes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 
 import '../bloc/game/game.dart';
 import '../models/ttt_board.dart';
+
+// TODO: Add a BlocListener to display a win/lose/tie dialog.
+// TODO: Add a refresh button to the app bar.
+
+// TODO: Show Russell published TTT Flutter code.
+
+// TODO: Break out cells and boards into widgets.
 
 class HomePage extends StatelessWidget {
   static const cellSpacing = 8.0;
@@ -14,47 +20,46 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Tic-Tac-Toe")),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: BlocBuilder<GameBloc, GameState>(
-                buildWhen: (previous, current) => previous.currentPlayer != current.currentPlayer,
-                builder: (context, state) {
-                  return Text(
-                    "Player: ${state.currentPlayer.name}",
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .headline5,
-                  );
-                },
+            Expanded(
+              child: Center(
+                child: BlocBuilder<GameBloc, GameState>(
+                  buildWhen: (previous, current) => previous.currentPlayer != current.currentPlayer,
+                  builder: (context, state) {
+                    return Text(
+                      "Player: ${state.currentPlayer.name}",
+                      style: Theme.of(context).textTheme.headline5,
+                    );
+                  },
+                ),
               ),
             ),
             Expanded(
+              flex: 8,
               child: BlocBuilder<GameBloc, GameState>(
                 buildWhen: (previous, current) => previous.board != current.board,
                 builder: (context, state) {
-                  return LayoutGrid(
-                    columnSizes: [1.fr, 1.fr, 1.fr],
-                    rowSizes: const [auto, auto, auto],
-                    rowGap: cellSpacing,
-                    columnGap: cellSpacing,
+                  return GridView.count(
+                    primary: false,
+                    padding: const EdgeInsets.all(16),
+                    crossAxisSpacing: cellSpacing,
+                    mainAxisSpacing: cellSpacing,
+                    crossAxisCount: 3,
                     children: [
                       for (int i = 0; i < 9; i++) GestureDetector(
                         onTap: () => context.read<GameBloc>().move(i),
                         child: Container(
-                          color: Colors.grey,
-                          child: state.board[i] == CellType.empty
-                              ? null
-                              : Center(
-                                  child: Text(
-                                    state.board[i].name,
-                                    style: Theme.of(context).textTheme.headline1,
-                                  ),
-                              ),
+                          padding: const EdgeInsets.all(8),
+                          color: Colors.black,
+                          child: FittedBox(
+                            child: Text(
+                              state.board[i].toCellString(),
+                              style: const TextStyle(color: Colors.grey),
+                            ),
+                          ),
                         ),
                       ),
                     ],
